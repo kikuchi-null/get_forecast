2e8f709a80591971e7b32d5eb34a6178a0474c54
taikikikuchi <taiki.k0124@gmail.com>
2024-05-03 20:52:29 +0900

    URL、郵便番号、APIキーはまとめて1つのファイルで管理する

--- lambda_function.py
+++ lambda_function.py
@@ -1,18 +1,15 @@
+# エンドポイント、郵便番号、APIキーは別ファイルで管理する
+# 上記の情報はgitのバージョン管理の対象外
 import json
 from GetForecast import HTTPCallout, MessageUtil
-from constants import POSTCODE, OPEN_WEATHER_MAP_KEY, LINE_TOKEN
+from constants import HEART_RAILS_GEO, POSTCODE, OPEN_WEATHER_MAP, OPEN_WEATHER_MAP_KEY, LINE, LINE_TOKEN 
 
 def lambda_handler(event, context):
-
-    # ENDPOINT
-    HEART_RAILS_GEO = "https://geoapi.heartrails.com/api/json" # https://geoapi.heartrails.com/
-    OPEN_WEATHER_MAP = "https://api.openweathermap.org/data/2.5/onecall" # https://openweathermap.org/forecast5
-    LINE = "https://notify-api.line.me/api/notify" # https://notify-bot.line.me/ja/
 
     """
     現在地の取得
     """
-    location = HTTPCallout(HEART_RAILS_GEO)
+    location = HTTPCallout(HEART_RAILS_GEO) # https://geoapi.heartrails.com/api/json
     
     # パラメータ設定
     params  = {
@@ -32,7 +29,7 @@ def lambda_handler(event, context):
     """
     天気予報の取得
     """
-    forecast = HTTPCallout(OPEN_WEATHER_MAP)
+    forecast = HTTPCallout(OPEN_WEATHER_MAP) # https://api.openweathermap.org/data/2.5/onecall
 
     # パラメーター設定
     params = {
@@ -56,7 +53,7 @@ def lambda_handler(event, context):
     LINE送信
     """
     mu = MessageUtil(weather_data)
-    line = HTTPCallout(LINE)
+    line = HTTPCallout(LINE) # https://notify-api.line.me/api/notify
 
     # メッセージ作成
     mu.add_message("場所", city_town)
